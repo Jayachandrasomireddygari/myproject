513d45bb4ee0d670b24c7ebb168e9d2687e82f76
jayachandrasomireddygari <jayachandra.somireddygari>
2025-09-10 15:56:38 +0530

    all files deleted

--- Tax_Audit/my_tax_audit_project/tax_app/utils.py
+++
@@ -1,5 +0,0 @@
-def calculate_tax(income, expenses, deductions):
-    taxable_income = income - expenses - deductions
-    tax_rate = 0.2  # Example flat tax rate
-    tax_due = taxable_income * tax_rate
-    return max(tax_due, 0)
